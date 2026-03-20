fbb3f9e3b8ce7cc87d7ea1cec85d7ca6986b510c
leeruochen <leeruochen7@gmail.com>
2026-03-20 14:25:19 +0800

    dd

---
+++ libgdx/assets/maps/16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="16x16" tilewidth="16" tileheight="16" tilecount="2192" columns="16">
+ <image source="../../../../../Free - Raven Fantasy Icons/Free - Raven Fantasy Icons/Full Spritesheet/16x16.png" width="256" height="2192"/>
+</tileset>
---
+++ libgdx/assets/maps/64x64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="64x64" tilewidth="64" tileheight="64" tilecount="2192" columns="16">
+ <image source="../../../../../Free - Raven Fantasy Icons/Free - Raven Fantasy Icons/Full Spritesheet/64x64.png" width="1024" height="8768"/>
+</tileset>
