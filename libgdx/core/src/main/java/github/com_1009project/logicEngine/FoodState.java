3d4766a5241a0e02b9f0b81d37dc3dc1876b9626
fanight7109 <57785300+fanight7109@users.noreply.github.com>
2026-03-20 17:03:55 +0800

    Add Ingredient Class

---
+++ libgdx/core/src/main/java/github/com_1009project/logicEngine/FoodState.java
@@ -0,0 +1,7 @@
+package github.com_1009project.logicEngine;
+
+public enum FoodState {
+    Raw,
+    Cooked,
+    Overcooked
+}
---
+++ libgdx/core/src/main/java/github/com_1009project/logicEngine/Ingredient.java
@@ -0,0 +1,40 @@
+package github.com_1009project.logicEngine;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import github.com_1009project.abstractEngine.Entity;
+
+public abstract class Ingredient extends Entity {
+    private String name;
+    private FoodState state;
+    private Texture texture;
+
+    public Ingredient(String name, FoodState state, Texture texture) {
+        this.name = name;
+        this.state = state;
+        this.texture = texture;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public FoodState getState() {
+        return state;
+    }
+
+    public void setState(FoodState state) {
+        this.state = state;
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, this.getPosition().x, this.getPosition().y, this.getSize().x, this.getSize().y);
+    }
+}
