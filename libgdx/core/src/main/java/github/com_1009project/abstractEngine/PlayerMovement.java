d3cb5cfe3dbdb888d32f29dc8d3e5220c3088eb0
xchin02 <83194359+xchin02@users.noreply.github.com>
2026-03-21 11:18:32 +0800

    Implement player movement handling for player

---
+++ libgdx/core/src/main/java/github/com_1009project/abstractEngine/PlayerMovement.java
@@ -0,0 +1,76 @@
+package github.com_1009project.abstractEngine;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class PlayerMovement {
+	private MovementComponent movementComponent;
+	
+    protected void handlePlayerInput(DynamicEntity entity, Event event, boolean isUp) {
+        movementComponent = entity.getMovementComponent();
+        if (entity == null) {
+        	return;
+        }
+        // Update input state
+        if (!isUp) { // Key Pressed
+            switch (event) {
+                case PlayerLeft: 
+                	movementComponent.setLeftPressed(true);
+                	break;
+                case PlayerRight: 
+                	movementComponent.setRightPressed(true);
+                	break;
+                case PlayerUp: 
+                	movementComponent.setUpPressed(true);
+                	break;
+                case PlayerDown: 
+                	movementComponent.setDownPressed(true);
+                	break;
+            }
+        } else { // Key Released
+            switch (event) {
+            	case PlayerLeft: 
+            		movementComponent.setLeftPressed(false);
+            		break;
+            	case PlayerRight: 
+            		movementComponent.setRightPressed(false);
+            		break;
+            	case PlayerUp: 
+            		movementComponent.setUpPressed(false);
+            		break;
+            	case PlayerDown: 
+            		movementComponent.setDownPressed(false);
+            		break;
+            }
+        }
+
+        // Update velocity based on current input state
+        updatePlayerVelocity(entity);
+    }
+
+    private void updatePlayerVelocity(DynamicEntity entity) {
+    	//the input events is faster than testEntity to run its constructor finish, preventing crash
+    	if (entity.getMovementComponent() == null) {
+    		return;
+    	}
+        Vector2 vel = entity.getMovementComponent().getVelocity();
+        
+        // Horizontal movement
+        if (movementComponent.isLeftPressed() && !movementComponent.isRightPressed()) {
+            vel.x = -movementComponent.getPlayerSpeed();
+        } else if (movementComponent.isRightPressed() && !movementComponent.isLeftPressed()) {
+            vel.x = movementComponent.getPlayerSpeed();
+        } else {
+            vel.x = 0; // Both or neither pressed
+        }
+        
+        // Vertical movement
+        if (movementComponent.isUpPressed() && !movementComponent.isDownPressed()) {
+            vel.y = movementComponent.getPlayerSpeed();
+        } else if (movementComponent.isDownPressed() && !movementComponent.isUpPressed()) {
+            vel.y = -movementComponent.getPlayerSpeed();
+        } else {
+            vel.y = 0; // Both or neither pressed
+        }
+        
+    }
+}
