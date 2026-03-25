98d7f1447adbbca1a5d65a762ead0c43cac24d25
fanight7109 <tan_jonathan@yahoo.com.sg>
2026-03-25 16:07:52 +0800

    Removed global assetmanager

--- libgdx/core/src/main/java/github/com_1009project/logicEngine/factories/CheeseFactory.java
+++ libgdx/core/src/main/java/github/com_1009project/logicEngine/factories/CheeseFactory.java
@@ -1,16 +1,22 @@
 package github.com_1009project.logicEngine.factories;
 
-import github.com_1009project.abstractEngine.Assets;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 
 import github.com_1009project.logicEngine.entities.Cheese;
 
 public class CheeseFactory implements Ifactory<Cheese> {
 
+    private final AssetManager assetManager;
+
+    public CheeseFactory(AssetManager assetManager) {
+        this.assetManager = assetManager;
+    }
+
     @Override
     public Cheese createEntity(float x, float y, float width, float height) {
         // Create and return a new Cheese entity with default properties
-        return new Cheese(x, y, width, height, Assets.getAssetManager().get("food/cheese.png", Texture.class)); // Texture can be set later
+        return new Cheese(x, y, width, height, assetManager.get("food/Cheese.png", Texture.class)); // Texture can be set later
     }
 
 }
--- libgdx/core/src/main/java/github/com_1009project/logicEngine/factories/StoveFactory.java
+++ libgdx/core/src/main/java/github/com_1009project/logicEngine/factories/StoveFactory.java
@@ -1,16 +1,22 @@
 package github.com_1009project.logicEngine.factories;
 
-import github.com_1009project.abstractEngine.Assets;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 
 import github.com_1009project.logicEngine.entities.Stove;
 
 public class StoveFactory implements Ifactory<Stove> {
 
+    private final AssetManager assetManager;
+
+    public StoveFactory(AssetManager assetManager) {
+        this.assetManager = assetManager;
+    }
+
     @Override
     public Stove createEntity(float x, float y, float width, float height) {
         // Create and return a new Stove entity with default properties
-        return new Stove(x, y, width, height, Assets.getAssetManager().get("foodstations/stove.png", Texture.class)); // Texture can be set later
+        return new Stove(x, y, width, height, assetManager.get("foodstations/stove.png", Texture.class)); // Texture can be set later
     }
 
 }
